efb2b5aa6d476560cd548ff0f2886a35a0bfb20c
Grant <matteo.grantc@gmail.com>
2021-10-03 16:36:26 -0400

    new version of fast time travel

--- Assets/Resources/Scripts/Teleback.cs
+++ Assets/Resources/Scripts/Teleback.cs
@@ -4,35 +4,47 @@ using UnityEngine;
 
 public class Teleback : MonoBehaviour
 {
+    //public Camera camera;
     public GameObject pathIndicator;
     public int pathSizeFrames = 150;
+    public int travelBackSpeedFrames = 5;
     Pose[] prevPath;
     int top = 0;
     int oldTop = -1;
+    //Vector2 camSize;
     // Start is called before the first frame update
     void Start()
     {
+        //camSize = camera.v;
         prevPath = new Pose[pathSizeFrames];
     }
 
     // Update is called once per frame
     void Update()
     {
+        //camera.sensorSize = camSize;
         
         if (Input.GetKey(KeyCode.E))
         {
+            //camera.sensorSize = 0.49F*camSize;
             if (oldTop == -1)
             {
                 oldTop = top;
                 top = (top - 1 + pathSizeFrames) % pathSizeFrames;
 
             }
-            if (top != oldTop && prevPath[top]!=null)
+            if (prevPath[top]!=null)
             {
-                Destroy(prevPath[top].indicator);
                 this.transform.position = prevPath[top].position;
-                prevPath[top] = null;
-                top = (top - 1 + pathSizeFrames) % pathSizeFrames;
+                for (int i = 0; i < travelBackSpeedFrames && top != oldTop; i++)
+                {
+                    if (prevPath[top] != null)
+                    {
+                        Destroy(prevPath[top].indicator);
+                        prevPath[top] = null;
+                        top = (top - 1 + pathSizeFrames) % pathSizeFrames;
+                    }
+                }
             }
         } else
         {
